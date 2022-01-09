fdc2c274606e28ae3be9f854d318cf4702a6c126
RezaSantika <93566793+RezaSantika@users.noreply.github.com>
2022-01-09 21:22:46 +0700

    Add files via upload

---
+++ views/main.blade.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Welcome R.M Kartika</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="{{ asset('style/css/styles.css')}}" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Navigation-->
+        <nav class="navbar navbar-light bg-light static-top">
+            <div class="container">
+                <a class="btn btn-danger" href="#signup">R.M KARTIKA</a>
+                <a class="navbar-brand" href="https://goo.gl/maps/qh5a114gX2epHHFM9">Peta Lokasi</a>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container position-relative">
+                <div class="row justify-content-center">
+                    <div class="col-xl-6">
+                        <div class="text-center text-white">
+                            <!-- Page heading-->
+                            <h1 class="mb-5">Selamat Datang Di Rumah Makan Kartika</h1>
+                            <!-- Signup form-->
+                            <!-- * * * * * * * * * * * * * * *-->
+                            <!-- * * SB Forms Contact Form * *-->
+                            <!-- * * * * * * * * * * * * * * *-->
+                            <!-- This form is pre-integrated with SB Forms.-->
+                            <!-- To make this form functional, sign up at-->
+                            <!-- https://startbootstrap.com/solution/contact-forms-->
+                            <!-- to get an API token!-->
+                            <form class="form-subscribe" id="contactForm" data-sb-form-api-token="API_TOKEN">
+                                <!-- Email address input-->
+                                
+                                <!-- Submit success message-->
+                                <!---->
+                                <!-- This is what your users will see when the form-->
+                                <!-- has successfully submitted-->
+                                <div class="d-none" id="submitSuccessMessage">
+                                    <div class="text-center mb-3">
+                                        <div class="fw-bolder">Form submission successful!</div>
+                                        <p>To activate this form, sign up at</p>
+                                        <a class="text-white" href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                                    </div>
+                                </div>
+                                <!-- Submit error message-->
+                                <!---->
+                                <!-- This is what your users will see when there is-->
+                                <!-- an error submitting the form-->
+                                <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- Icons Grid-->
+        <section class="features-icons bg-light text-center">
+            <div class="container">
+                <h1>Daftar Menu</h1>
+                <div class="row">
+                    <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <div class="features-icons-icon d-flex"><i class="bi-layers m-auto text-primary"></i></div>
+                            <h3>Nasi Liwet Komplit</h3>
+
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <div class="features-icons-icon d-flex"><i class="bi-layers m-auto text-primary"></i></div>
+                            <h3>Macam-Macam Ikan</h3>
+                            
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <div class="features-icons-icon d-flex"><i class="bi-layers m-auto text-primary"></i></div>
+                            <h3>Sate</h3>
+                            
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <div class="features-icons-icon d-flex"><i class="bi-layers m-auto text-primary"></i></div>
+                            <h3>Paket Sayuran</h3>
+                           
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <<div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <div class="features-icons-icon d-flex"><i class="bi-layers m-auto text-primary"></i></div>
+                            <h3>Aneka Minuman</h3>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Image Showcases-->
+        <section class="showcase">
+            <div class="container-fluid p-0">
+                <div class="row g-0">
+                    <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('style/assets/img/img1.jpeg')"></div>
+                    <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                        <h2>Nasi Liwet Komplit</h2>
+                        <p class="lead mb-0">Nasi Liwet Polos : Rp.10K</p>
+                        <p class="lead mb-0">Nasi Liwet Komplit (A) : Rp.50K</p>
+                        <p class="lead mb-0">Nasi Liwet Komplit (B) : Rp.100K</p>
+                        <p class="lead mb-0">Nasi Liwet Komplit (C) : Rp.150K</p>
+                    </div>
+                </div>
+                <div class="row g-0">
+                    <div class="col-lg-6 text-white showcase-img" style="background-image: url('style/assets/img/ikanbakar.jpg')"></div>
+                    <div class="col-lg-6 my-auto showcase-text">
+                        <h2>Menu Ikan</h2>
+                        <p class="lead mb-0">Nila Asam Manis : Rp.85K</p>
+                        <p class="lead mb-0">Nila Bakar : Rp.80K</p>
+                        <p class="lead mb-0">Nila Goreng : Rp.80K</p>
+                        <p class="lead mb-0">Nila Cobek : Rp.85K</p>
+                        <p class="lead mb-0">Gurame Asam Manis : Rp.90K</p>
+                        <p class="lead mb-0">Gurame Bakar : Rp.85K</p>
+                        <p class="lead mb-0">Gurame Goreng : Rp.85K</p>
+                        <p class="lead mb-0">Gurame Cobek : Rp.90K</p>
+                        <p class="lead mb-0">Ikan Mas Asam Manis : Rp.85K</p>
+                        <p class="lead mb-0">Ikan Mas Bakar : Rp.80K</p>
+                        <p class="lead mb-0">Ikan Mas Goreng : Rp.80K</p>
+                        <p class="lead mb-0">Ikan Mas Cobek : Rp.85K</p>
+                    </div>
+                </div>
+                <div class="row g-0">
+                    <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('style/assets/img/img3.jpeg')"></div>
+                    <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                        <h2>Menu Sate</h2>
+                        <p class="lead mb-0">Sate Sapi (10 tusuk/porsi) : Rp.40K</p>
+                        <p class="lead mb-0">Sate Kambing (10 tusuk/porsi) : Rp.40K</p>
+                    </div>
+                </div>
+                <div class="row g-0">
+                    <div class="col-lg-6 text-white showcase-img" style="background-image: url('style/assets/img/img5.jpg')"></div>
+                    <div class="col-lg-6 my-auto showcase-text">
+                        <h2>Menu Sayur-Sayuran</h2>
+                        <p class="lead mb-0">Sayur Asem: Rp.12K</p>
+                        <p class="lead mb-0">Sop Gurame : Rp.35K</p>
+                        <p class="lead mb-0">Sop Ayam : Rp.32K</p>
+                        <p class="lead mb-0">Sop Buntut : Rp.40K</p>
+                        <p class="lead mb-0">Sop Kaki : Rp.40K</p>
+                        <p class="lead mb-0">Sop Iga : Rp.40K</p>
+                        <p class="lead mb-0">Soto Daging Sapi : Rp.40K</p>
+                        <p class="lead mb-0">Gule Sapi : Rp.40K</p>
+                        <p class="lead mb-0">Gule Kambing : Rp.32K</p>
+                    </div>
+                </div>
+                <div class="row g-0">
+                    <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('style/assets/img/img6.jpg')"></div>
+                    <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                        <h2>Aneka Juice</h2>
+                        <p class="lead mb-0">Juice Melon : Rp.15K</p>
+                        <p class="lead mb-0">Juice Tomat : Rp.15K</p>
+                        <p class="lead mb-0">Juice Alpukat : Rp.17K</p>
+                        <p class="lead mb-0">Juice Jambu : Rp.15K</p>
+                        <p class="lead mb-0">Juice Sirsak : Rp.15K</p>
+                        <p class="lead mb-0">Juice Wortel : Rp.15K</p>
+                        <p class="lead mb-0">Juice Timun : Rp.15K</p>
+                        <p class="lead mb-0">Juice Strawberry : Rp.17K</p>
+                        <p class="lead mb-0">Mix Juice : Rp.17K</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Testimonials-->
+        <section class="testimonials text-center bg-light">
+            <div class="container">
+                <h2 class="mb-5">Sponsor Shipt RM.KARTIKA...</h2>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                            <img class="img-fluid rounded-circle mb-3" src="{{asset('style/assets/img/owner kartika.jpg')}}" alt="..." />
+                            <h5>Ibu Hajjah Yoyoh Yohaeni</h5>
+                            <p class="font-weight-light mb-0">"Owner RM. Kartika Sumedang "</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                            <img class="img-fluid rounded-circle mb-3" src="{{asset('style/assets/img/Staff.jpg')}}" alt="..." />
+                            <h5>Dadang Suhendar</h5>
+                            <p class="font-weight-light mb-0">"Staff Manajemen RM. Kartika Sumedang "</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                            <img class="img-fluid rounded-circle mb-3" src="{{asset('style/assets/img/eza.jpeg')}}" alt="..." />
+                            <h5>Reza Santika</h5>
+                            <p class="font-weight-light mb-0">"Developer Website Aplikasi RM. Kartika Sumedang "</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Call to Action-->
+        <section class="call-to-action text-white text-center" id="signup">
+            <div class="container position-relative">
+                <div class="row justify-content-center">
+                    <div class="col-xl-6">
+                        <h2 class="mb-4">Yuk Pesan Sekarang</h2>
+   
+                        <form class="form-subscribe" id="contactFormFooter" data-sb-form-api-token="API_TOKEN">
+                            <!-- Email address input-->
+                            <div class="row">
+                                <div class="col">
+                                </div>
+                                <div><button class="btn btn-success " >WhatsApp</button> <h2>085795939909</h2> </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="footer bg-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 h-100 text-center text-lg-start my-auto">
+                        <ul class="list-inline mb-2">
+                            <li class="list-inline-item"><a href="#!">About</a></li>
+                            <li class="list-inline-item">⋅</li>
+                            <li class="list-inline-item"><a href="#!">Contact</a></li>
+                            <li class="list-inline-item">⋅</li>
+                            <li class="list-inline-item"><a href="#!">Terms of Use</a></li>
+                            <li class="list-inline-item">⋅</li>
+                            <li class="list-inline-item"><a href="#!">Privacy Policy</a></li>
+                        </ul>
+                        <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2021. All Rights Reserved.</p>
+                    </div>
+                    <div class="col-lg-6 h-100 text-center text-lg-end my-auto">
+                        <ul class="list-inline mb-0">
+                            <li class="list-inline-item me-4">
+                                <a href="#!"><i class="bi-facebook fs-3"></i></a>
+                            </li>
+                            <li class="list-inline-item me-4">
+                                <a href="#!"><i class="bi-twitter fs-3"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#!"><i class="bi-instagram fs-3"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
